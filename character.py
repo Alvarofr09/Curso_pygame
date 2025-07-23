8cf488ed932c604b39f654ade1e8cf7f8607c311
Alvarofr09 <127234472+Alvarofr09@users.noreply.github.com>
2025-07-23 16:47:38 +0200

    variable nueva para las animaciones y para voltear la img. Metodos de actualizar la img para la animacion y cambiar le metodo move para que gira la imagen

--- character.py
+++ character.py
@@ -2,13 +2,35 @@ import pygame
 import constantes
 
 class Character():
-	def __init__(self, x, y):
+	def __init__(self, x, y, animations):
+		self.flip = False
+		self.animations = animations
+		# Imagen de la animacion que se esta mostrando
+		self.frame_index = 0
+		#Almacenar la hra actual en ms
+		self.update_time = pygame.time.get_ticks()
+		self.image = animations[self.frame_index]
 		self.shape = pygame.Rect(0, 0, constantes.WIDTH_CHARACTER, constantes.HEIGHT_CHARACTER)
 		self.shape.center = (x, y)
 
+	def update(self):
+		cooldown_animation = 100
+		self.image = self.animations[self.frame_index]
+		if pygame.time.get_ticks() - self.update_time >= cooldown_animation:
+			self.frame_index = self.frame_index + 1
+			self.update_time = pygame.time.get_ticks()
+		if self.frame_index >= len(self.animations):
+			self.frame_index = 0
+
 	def draw(self, window):
-		pygame.draw.rect(window, constantes.COLOR_CARACTER, self.shape)
+		image_flip = pygame.transform.flip(self.image, self.flip, False)
+		window.blit(image_flip, self.shape)
+		#pygame.draw.rect(window, constantes.COLOR_CARACTER, self.shape, 1)
 
 	def move(self, delta_x, delta_y):
+		if delta_x < 0:
+			self.flip = True
+		if delta_x > 0:
+			self.flip = False
 		self.shape.x = self.shape.x + delta_x
 		self.shape.y = self.shape.y + delta_y
