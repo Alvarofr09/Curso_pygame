55f393710179fe4f1fc1289d828f9da268706670
Alvarofr09 <127234472+Alvarofr09@users.noreply.github.com>
2025-07-25 17:12:53 +0200

    Clase bala y sus funciones. Y creaciones de nuevas variables necesarias para la bala

--- weapons.py
+++ weapons.py
@@ -3,13 +3,18 @@ import constantes
 import math
 
 class Weapon():
-	def __init__(self, image):
+	def __init__(self, image, image_bullet):
+		self.image_bullet = image_bullet
 		self.image_original = image
 		self.angle = 0
 		self.image = pygame.transform.rotate(self.image_original, self.angle)
 		self.shape = self.image.get_rect()
+		self.shot = False
+		self.last_shot = pygame.time.get_ticks()
 
 	def update(self, character):
+		shot_cooldown = constantes.BULLET_COOLDOWN
+		bullet = None
 		self.shape.center = character.shape.center
 		if (character.flip):
 			self.shape.x = self.shape.x - character.shape.width/2
@@ -34,6 +39,16 @@ class Weapon():
 			if self.angle < -155:
 				self.angle = -155
 
+		#Detectar click del mouse
+		#[0] izquierdo, [1] central, [2] derecho
+		if pygame.mouse.get_pressed()[0] and self.shot == False and (pygame.time.get_ticks() - self.last_shot >= shot_cooldown):
+			bullet = Bullet(self.image_bullet, self.shape.centerx, self.shape.centery, self.angle)
+			self.shot = True
+			self.last_shot = pygame.time.get_ticks()
+		if pygame.mouse.get_pressed()[0] == False:
+			self.shot = False
+		return bullet
+
 	def flip(self, flip):
 		if (flip):
 			img_flip = pygame.transform.flip(self.image_original, True, False)
@@ -46,3 +61,25 @@ class Weapon():
 		self.image = pygame.transform.rotate(self.image, self.angle)
 		window.blit(self.image, self.shape)
 		#pygame.draw.rect(window, constantes.COLOR_WEAPON, self.shape, 1)
+
+
+class Bullet(pygame.sprite.Sprite):
+	def __init__(self, image, x, y, angle):
+		pygame.sprite.Sprite.__init__(self)
+		self.image_original = image
+		self.angle = angle
+		self.image = pygame.transform.rotate(self.image_original, self.angle)
+		self.rect = self.image.get_rect()
+		self.rect.center = (x, y)
+		self.delta_x = math.cos(math.radians(self.angle)) * constantes.BULLET_VELOZ
+		self.delta_y = -math.sin(math.radians(self.angle)) * constantes.BULLET_VELOZ
+
+	def update(self):
+		self.rect.x += self.delta_x
+		self.rect.y = self.rect.y + self.delta_y
+
+		if self.rect.right < 0 or self.rect.left > constantes.WIDTH_SCREEN or self.rect.bottom < 0 or self.rect.top > constantes.HEIGHT_SCREEN:
+			self.kill()
+
+	def draw(self, window):
+		window.blit(self.image, (self.rect.centerx, self.rect.centery - int(self.image.get_height())))
